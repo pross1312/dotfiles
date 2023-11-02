869273e23d917abec8eed83862ed4e244fc350e8
pross1312 <dangvinhtuong12@gmail.com>
2023-11-02 22:12:42 +0700

    refactor, add cyperpunk st

---
+++ st/tokyonigh.h
@@ -0,0 +1,30 @@
+static const char *colorname[] = { // tokyonight
+    [0] = "#15161e",
+    [1] = "#f7768e",
+    [2] = "#9ece6a",
+    [3] = "#e0af68",
+    [4] = "#7aa2f7",
+    [5] = "#bb9af7",
+    [6] = "#7dcfff",
+    [7] = "#a9b1d6",
+    [8] = "#414868",
+    [9] = "#f7768e",
+    [10] = "#9ece6a",
+    [11] = "#e0af68",
+    [12] = "#7aa2f7",
+    [13] = "#bb9af7",
+    [14] = "#7dcfff",
+    [15] = "#c0caf5",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"#c0caf5", /* default foreground colour */
+	"#1a1b26", /* default background colour */
+};
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
