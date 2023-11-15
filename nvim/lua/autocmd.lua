40e1fc7c066cd59aafe6f3ff0e09570d5ad455c3
pross1312 <dangvinhtuong12@gmail.com>
2023-11-15 23:14:38 +0700

    update netrw

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -58,24 +58,40 @@ vim.api.nvim_create_autocmd('BufEnter', {
 --     end
 -- })
 vim.api.nvim_create_augroup('NetrwGroup', {clear = true})
+vim.api.nvim_create_autocmd('VimEnter', {
+    pattern = "*",
+    callback = function()
+        if vim.fn.argc() == 0 then
+            vim.cmd "Explore"
+        end
+    end,
+    group = 'NetrwGroup'
+})
+function netrw_split_open(file)
+    local last_window = vim.fn.winnr('$') == vim.fn.winnr()
+    if vim.fn.isdirectory(file) == 1  or not last_window then
+        vim.call('netrw#LocalBrowseCheck', vim.call('netrw#Call', 'NetrwBrowseChgDir', 1, file))
+    else
+        vim.cmd "vsplit | vertical resize +35"
+        vim.call('netrw#LocalBrowseCheck', vim.call('netrw#Call', 'NetrwBrowseChgDir', 1, file))
+    end
+end
 vim.api.nvim_create_autocmd('FileType', {
     pattern = "netrw",
     callback = function()
         local map = vim.keymap.set
+        -- vim.cmd 'nnoremap <buffer> cd  :execute "cd ".b:netrw_curdir<cr>:pwd<cr>'
+        map("n", "n", function()
+            local file = vim.fn.input("Enter file name: ", "", "file")
+            netrw_split_open(file)
+        end, {silent = true, buffer = true})
         map("n", "<CR>", function()
-            local last_window = vim.fn.winnr('$') == vim.fn.winnr()
-            local file = vim.fn.expand('<cfile>:p')
-            if vim.fn.isdirectory(file) == 1  or not last_window then
-                vim.call('netrw#LocalBrowseCheck', vim.call('netrw#Call', 'NetrwBrowseChgDir', 1, vim.call('netrw#Call', 'NetrwGetWord')))
-            else
-                vim.cmd "vsplit | vertical resize +35"
-                vim.call('netrw#LocalBrowseCheck', vim.call('netrw#Call', 'NetrwBrowseChgDir', 1, vim.call('netrw#Call', 'NetrwGetWord')))
-            end
+            local file = vim.call('netrw#Call', 'NetrwGetWord')
+            netrw_split_open(file)
         end, {silent = true, buffer = true, nowait = true})
     end,
     group = 'NetrwGroup'
 })
-
 local aug = vim.api.nvim_create_augroup("buf_large", { clear = true })
 vim.api.nvim_create_autocmd({ "BufReadPre" }, {
     callback = function()
