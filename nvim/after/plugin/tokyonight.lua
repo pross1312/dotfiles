471f0343537eda24cf667497fc56151fe33e3131
pross1312 <dangvinhtuong12@gmail.com>
2024-05-19 22:17:46 +0700

    update nvim, add build and run ( && )

--- nvim/after/plugin/tokyonight.lua
+++ nvim/after/plugin/tokyonight.lua
@@ -1,14 +1,14 @@
-require("tokyonight").setup({
+if vim.fn.is_module_available("tokyonight") then require("tokyonight").setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
     style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     light_style = "day", -- The theme is used when the background is set to light
     transparent = false, -- Enable this to disable setting the background color
-    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+    terminal_colors = false, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
     styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = false },
+        comments = { italic = true },
         keywords = { italic = false },
         functions = {},
         variables = {},
@@ -20,21 +20,22 @@ require("tokyonight").setup({
     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
     dim_inactive = false, -- dims inactive windows
-    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
     ---@param colors ColorScheme
-    on_colors = function(colors) end,
+    --- on_colors = function(colors) end,
 
     --- You can override specific highlights to use other groups or a hex color
     --- function will be called with a Highlights and ColorScheme table
     ---@param highlights Highlights
     ---@param colors ColorScheme
-    on_highlights = function(highlights, colors)
-        highlights.CursorLine   = { bg = ""}
-        highlights.CursorLineNr = { fg = "#737afa" }
-        highlights.SignColumn = { bg = "" }
-        highlights.LineNr = { fg = "#4c5372" }
-    end,
+    -- on_highlights = function(highlights, colors)
+        -- highlights.CursorLine   = { bg = ""}
+        -- highlights.CursorLineNr = { fg = "#737afa" }
+        -- highlights.SignColumn = { bg = "" }
+        -- highlights.LineNr = { fg = "#4c5372" }
+    -- end,
 })
+end
